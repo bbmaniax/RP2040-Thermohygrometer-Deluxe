7c6b16c86db16a4021aa1c9dc43d91e20506ef77
Bread Board Maniax <bbmaniax@users.noreply.github.com>
2025-10-29 06:29:38 +0900

    refactor: main

--- RP2040-Thermohygrometer-Deluxe.ino
+++ RP2040-Thermohygrometer-Deluxe.ino
@@ -1,6 +1,7 @@
 // RP2040-Thermohygrometer-Deluxe
 
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 #include <Adafruit_SSD1306.h>
 
 #include "AHTX0.h"
@@ -17,6 +18,7 @@
 #include "TimeKeeper.h"
 #include "View.h"
 
+#define RGBLED_PIN 16
 #define BUTTON_PIN 29
 #define I2C1_SDA_PIN 28
 #define I2C1_SCL_PIN 14
@@ -28,10 +30,12 @@
 #define DISPLAY_WIDTH 128
 #define DISPLAY_HEIGHT 32
 
-#define HISTORY_BUFFER_SIZE (DISPLAY_WIDTH / (PLOT_HORIZONTAL_SPACING + 1) + PLOT_HORIZONTAL_SPACING)
 #define SENSOR_READ_INTERVAL_MS 3000
 #define PLOT_HORIZONTAL_SPACING 1
 
+#define HISTORY_BUFFER_SIZE (DISPLAY_WIDTH / (PLOT_HORIZONTAL_SPACING + 1) + PLOT_HORIZONTAL_SPACING)
+
+SensorManager::SensorData sensorData;
 uint8_t displayBuffer1[DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
 uint8_t displayBuffer2[DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
 uint8_t displayBuffer3[DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
@@ -41,16 +45,15 @@ int16_t pressureHistoryBuffer[HISTORY_BUFFER_SIZE];
 History temperatureHistory(temperatureHistoryBuffer, HISTORY_BUFFER_SIZE);
 History humidityHistory(humidityHistoryBuffer, HISTORY_BUFFER_SIZE);
 History pressureHistory(pressureHistoryBuffer, HISTORY_BUFFER_SIZE);
-SensorManager::SensorData sensorData;
 
+Adafruit_NeoPixel rgbled(1, RGBLED_PIN, NEO_GRB + NEO_KHZ800);
+Button button(BUTTON_PIN);
 SoftI2C softI2C1(I2C1_SDA_PIN, I2C1_SCL_PIN);
 SoftI2C softI2C2(I2C2_SDA_PIN, I2C2_SCL_PIN);
 SoftI2C softI2C3(I2C3_SDA_PIN, I2C3_SCL_PIN);
 SoftI2CWire wire1(softI2C1);
 SoftI2CWire wire2(softI2C2);
 SoftI2CWire wire3(softI2C3);
-Button button(BUTTON_PIN);
-TimeKeeper timeKeeper(SENSOR_READ_INTERVAL_MS);
 AHTX0 thermometer(wire1);
 BMP280 barometer(wire1);
 SSD1306 oled1(wire1, DISPLAY_WIDTH, DISPLAY_HEIGHT);
@@ -59,32 +62,37 @@ SSD1306 oled3(wire3, DISPLAY_WIDTH, DISPLAY_HEIGHT);
 SSD1306Display display1(oled1, DISPLAY_WIDTH, DISPLAY_HEIGHT, displayBuffer1);
 SSD1306Display display2(oled2, DISPLAY_WIDTH, DISPLAY_HEIGHT, displayBuffer2);
 SSD1306Display display3(oled3, DISPLAY_WIDTH, DISPLAY_HEIGHT, displayBuffer3);
-
 SensorManager sensorManager(thermometer, barometer);
+TimeKeeper timeKeeper(SENSOR_READ_INTERVAL_MS);
 
 Model model(temperatureHistory, humidityHistory, pressureHistory);
 View view(model, display1, display2, display3, DISPLAY_WIDTH, DISPLAY_HEIGHT);
 
 void setup() {
-  DEBUG_SERIAL_BEGIN(9600);
-  DEBUG_SERIAL_WAIT_FOR();
-  DEBUG_SERIAL_PRINTLN();
-  DEBUG_SERIAL_PRINTLN("--");
-  DEBUG_SERIAL_PRINTLN("Thermohygrometer Deluxe");
+  rgbled.begin();
+  rgbled.setBrightness(127);
+  blink(160, 160, 0, 200);
+  blink(160, 160, 0, 200);
+  blink(160, 160, 0, 200);
+
+  Serial.begin(9600);
+  while (!Serial && millis() < 1000) {}
+  Serial.println();
+  Serial.println("--");
+  Serial.println("Thermohygrometer Deluxe (build " + timestamp() + ")");
 
   button.begin();
-  timeKeeper.begin();
-
   wire1.begin();
   wire2.begin();
   wire3.begin();
   delay(100);
 
   sensorManager.begin();
-  sensorManager.acquire(&sensorData);
+  timeKeeper.begin();
   model.begin(sensorData.temperature, sensorData.humidity, sensorData.pressure);
-
   view.begin(DISPLAY_I2C_ADDRESS);
+
+  sensorManager.acquire(&sensorData);
   delay(1000);
 
   if (digitalRead(BUTTON_PIN) == LOW) {
@@ -122,12 +130,34 @@ void loop() {
   }
 
   if (needUpdate) {
-    // DEBUG_SERIAL_PRINTLN("Time to render views");
+    Serial.println("T:" + String(sensorData.temperature / 10.0f, 1) + " H:" + String(sensorData.humidity / 10.0f, 1) + " P:" + String(sensorData.pressure / 10.0f, 1));
     view.render();
     needUpdate = false;
   }
 
   delay(10);
+}
+
+void blink(uint8_t r, uint8_t g, uint8_t b, unsigned long durationMs) {
+  rgbled.setPixelColor(0, r, g, b);
+  rgbled.show();
+  delay(durationMs);
+  rgbled.setPixelColor(0, 0, 0, 0);
+  rgbled.show();
+  delay(durationMs);
+}
+
+String timestamp() {
+  const char* m = "JanFebMarAprMayJunJulAugSepOctNovDec";
+  char mon[4];
+  int d, y, hh, mm, ss;
+  sscanf(__DATE__, "%3s %d %d", mon, &d, &y);
+  sscanf(__TIME__, "%d:%d:%d", &hh, &mm, &ss);
+  int mo = (strstr(m, mon) - m) / 3 + 1;
+
+  char buf[16];
+  sprintf(buf, "%04d%02d%02d.%02d%02d%02d", y, mo, d, hh, mm, ss);
+  return String(buf);
 }
 
 void scan(AbstractWire& wire, SSD1306Display& display) {
